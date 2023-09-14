20bd3e83552060b5220b9bc5fd610429e4d88125
TaysWeb <snelltararobin@gmail.com>
2023-09-14 11:49:42 +0200

    added a dbURL

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const {createPool} = require('mysql');
 require('dotenv').config();
 
 const db = createPool({
+    url:process.env.DB_URL,
     host: process.env.DB_HOST ,
     user : process.env.DB_USER,
     password : process.env.DB_PASS, 
