b762cfeef7d7ca176b47f8ca37a6f8314a7921c3
TaysWeb <snelltararobin@gmail.com>
2023-09-14 10:36:17 +0200

    changes to db if

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
 const {createPool} =require( 'mysql');
 require('dotenv/config')
 
-const Connection = createPool({
+const db = createPool({
     host: process.env.DB_HOST ,
     user : process.env.DB_USER,
     password : process.env.DB_PASS, 
     database : process.env.DB_NAME,
     connectionLimit : 55
 }) 
-Connection.getConnection((err) => {
+db.getConnection((err) => {
   if (err) throw err ;   
-  console.log("Database Connected successfully!");
+  console.log("Database Connected successfully!")
 });
 
-module.exports = Connection ;
+module.exports = db;
