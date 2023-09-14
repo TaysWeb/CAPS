7edecbf928de5bc9d5c77cc373acc7eada381b67
TaysWeb <snelltararobin@gmail.com>
2023-09-14 16:07:50 +0200

    commented the stuff back

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,11 @@ const {createPool} = require('mysql');
 require('dotenv').config();
 
 const db = createPool({
-    url:process.env.DB_URL,
-    // host: process.env.DB_HOST ,
-    // user : process.env.DB_USER,
-    // password : process.env.DB_PASS, 
-    // database : process.env.DB_NAME,
-    // port : process.env.DB_PORT, 
+    host: process.env.DB_HOST ,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS, 
+    database : process.env.DB_NAME,
+    port : process.env.DB_PORT, 
     connectionLimit : 55
 }) 
 db.getConnection((err) => {
