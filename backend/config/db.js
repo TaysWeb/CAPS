68d674ba018b16689d9167d1bcefb6d121ac4d24
TaysWeb <130046806+TaysWeb@users.noreply.github.com>
2023-09-14 15:45:11 +0200

    Update db.js5

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 const db = createPool({
     url:process.env.DB_URL,
-    // host: process.env.DB_HOST ,
-    // user : process.env.DB_USER,
-    // password : process.env.DB_PASS, 
-    // database : process.env.DB_NAME,
-    // port : process.env.DB_PORT, 
+    host: process.env.DB_HOST ,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS, 
+    database : process.env.DB_NAME,
+    port : process.env.DB_PORT, 
     connectionLimit : 55
 }) 
 db.getConnection((err) => {
